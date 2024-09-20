641fad6477611d693de63c99b017db6df47f7e88
cloudinfer <2726012413@qq.com>
2024-09-20 07:12:16 +0000

    vae class

--- vae_1.py
+++ vae_1.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import v2
+import torch.nn.functional as F
+
+from dataclasses import dataclass
 
 
 if torch.cuda.is_available():
@@ -31,9 +34,96 @@ def train():
         shuffle=True
     )
 
+@dataclass
+class VAEOutput:
+    z_dist: torch.distributions.Distribution
+    z_sample: torch.Tensor
+    x_recon: torch.Tensor
+
+    loss: torch.Tensor
+    loss_recon: torch.Tensor
+    loss_kl: torch.Tensor
+
 class VAE(nn.Module):
     def __init__(self, input_dim, hidden_dim, latent_dim) -> None:
-        super(VAE, self).__init__()
+        super().__init__()
+
+        # encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
+            nn.SiLU(),
+            nn.Linear(hidden_dim, hidden_dim // 2),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 2, hidden_dim // 4),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 4, hidden_dim // 8),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 8, 2 * latent_dim)
+        )
+
+        self.softplus = nn.Softplus()
+
+        # decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim // 8),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 8, hidden_dim // 4),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 4, hidden_dim //2),
+            nn.SiLU(),
+            nn.Linear(hidden_dim // 2, hidden_dim),
+            nn.SiLU(),
+            nn.Linear(hidden_dim, input_dim),
+            nn.Sigmoid()
+        )
+
+    def encode(self, x, eps: float=1e-8):
+        x = self.encoder(x)
+        mu, logvar = torch.chunk(x, 2, dim=-1)
+        scale = self.softplus(logvar) + eps
+        scale_tril = torch.diag_embed(scale)
+
+        return torch.distributions.MultivariateNormal(mu, scale_tril=scale_tril)
+
+    def reparameterize(self, dist):
+        return dist.resample()
+
+    def decode(self, z):
+        return self.decoder(z)
+
+    def forward(self, x, compute_loss: bool=True):
+        dist = self.encode(x)
+        z = self.reparameterize(dist)
+        recon_x = self.decode(z)
+
+        if not compute_loss:
+            return VAEOutput(
+                z_dist=dist,
+                z_sample=z,
+                x_recon=recon_x,
+                loss=None,
+                loss_recon=None,
+                loss_kl=None
+            )
+
+        std_normal = torch.distributions.MultivariateNormal(
+            torch.zeros_like(z, device=z.device),
+            scale_tril=torch.eye(
+                z.shape[-1],
+                device=z.device()).unsqueeze(0).expand(z.shape[0], -1, -1)
+        )
+        loss_kl = torch.distributions.kl.kl_divergence(dist, std_normal).mean()
+        loss_recon = F.binary_cross_entropy(recon_x, x + 0.5, reduction="none").sum(-1).mean()
+        loss = loss_recon + loss_kl
+
+        return VAEOutput(
+            z_dist=dist,
+            z_sample=z,
+            x_recon=recon_x,
+            loss=loss,
+            loss_recon=loss_recon,
+            loss_kl=loss_kl
+        )
 
 
 if __name__ == "__main__":
