a339c54264048aa7b5bce5395038dfde689620a3
cloudinfer <2726012413@qq.com>
2024-09-23 03:17:46 +0000

    train

--- vae_1.py
+++ vae_1.py
@@ -6,33 +6,8 @@ from torchvision.transforms import v2
 import torch.nn.functional as F
 
 from dataclasses import dataclass
+from tqdm.auto import tqdm
 
-
-if torch.cuda.is_available():
-    device = torch.device("cuda:0")
-else:
-    device = torch.device("cpu")
-
-transform = v2.Compose([
-    v2.ToImage(),
-    v2.ToDtype(torch.float32, scale=True),
-    v2.Lambda(lambda x: x.view(-1) - 0.5)
-])
-
-def train():
-    batch_size = 256
-    # dimension=784: 28*28
-    train_data = datasets.MNIST(
-        "~/.pytorch/mnist_data",
-        download=True,
-        train=True,
-        transform=transform
-    )
-    train_loader = DataLoader(
-        train_data,
-        batch_size=batch_size,
-        shuffle=True
-    )
 
 @dataclass
 class VAEOutput:
@@ -86,7 +61,7 @@ class VAE(nn.Module):
         return torch.distributions.MultivariateNormal(mu, scale_tril=scale_tril)
 
     def reparameterize(self, dist):
-        return dist.resample()
+        return dist.rsample()
 
     def decode(self, z):
         return self.decoder(z)
@@ -110,7 +85,7 @@ class VAE(nn.Module):
             torch.zeros_like(z, device=z.device),
             scale_tril=torch.eye(
                 z.shape[-1],
-                device=z.device()).unsqueeze(0).expand(z.shape[0], -1, -1)
+                device=z.device).unsqueeze(0).expand(z.shape[0], -1, -1)
         )
         loss_kl = torch.distributions.kl.kl_divergence(dist, std_normal).mean()
         loss_recon = F.binary_cross_entropy(recon_x, x + 0.5, reduction="none").sum(-1).mean()
@@ -124,6 +99,68 @@ class VAE(nn.Module):
             loss_recon=loss_recon,
             loss_kl=loss_kl
         )
+
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
+
+transform = v2.Compose([
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Lambda(lambda x: x.view(-1) - 0.5)
+])
+
+def train():
+    # hyperparameters
+    batch_size = 256
+    learning_rate = 1e-3
+    weight_decay = 1e-2
+    num_epochs = 50
+    latent_dim = 2
+    hidden_dim = 512
+    # dimension=784: 28*28
+    train_data = datasets.MNIST(
+        "~/.pytorch/mnist_data",
+        download=True,
+        train=True,
+        transform=transform
+    )
+    train_loader = DataLoader(
+        train_data,
+        batch_size=batch_size,
+        shuffle=True
+    )
+    model = VAE(
+        input_dim=784,
+        hidden_dim=hidden_dim,
+        latent_dim=latent_dim
+    ).to(device)
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr=learning_rate,
+        weight_decay=weight_decay
+    )
+
+    model.train()
+
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch+1} / {num_epochs}")
+        for batch_idx, (data, _) in enumerate((train_loader)):
+            data = data.to(device)
+            optimizer.zero_grad()
+
+            output = model(data)
+            loss = output.loss
+            loss.backward()
+
+            if batch_idx % 50 == 0:
+                print(f"Epoch: {epoch + 1} Batch: {batch_idx + 1} Loss: {loss} Loss_kl: {output.loss_kl} Loss_recon: {output.loss_recon}")
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+
+
 
 
 if __name__ == "__main__":
